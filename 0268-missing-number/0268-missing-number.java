178c1597f36cb6abf2476453e8ced0550b824f85
Pranay2969 <mpranaykumarreddy781@gmail.com>
2026-09-09 23:08:51 +0530

    Time: 0 ms (100%), Space: 47.2 MB (65.89%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -3,7 +3,7 @@ class Solution {
         int n = nums.length;
         int miss = 0;
         for(int i = 1; i <= n; i++) {
-            miss = (miss ^ i) ^ nums[i - 1];
+            miss ^= i ^ nums[i - 1];
         }
 
         return miss;
